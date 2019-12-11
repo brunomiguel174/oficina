78111af273ed713e31f1dbb8022f18a465767792
brunomiguel174 <brunofernandes174@hotmail.com>
2019-12-11 17:34:51 +0000

    Update

--- oficina.sql
+++ oficina.sql
@@ -1,23 +1,22 @@
 use mind_oficina;
 
 
-DROP TABLE IF EXISTS carro;
-DROP TABLE IF EXISTS marca;
 DROP TABLE IF EXISTS modeloCarro_peca;
-DROP TABLE IF EXISTS pecas_utilizadas;
-DROP TABLE IF EXISTS peca;
-DROP TABLE IF EXISTS armazem;
 DROP TABLE IF EXISTS modelo;
 DROP TABLE IF EXISTS funcionario_horasDeTrabalho;
 DROP TABLE IF EXISTS reparacao;
 DROP TABLE IF EXISTS funcionario;
 DROP TABLE IF EXISTS especialidade;
+DROP TABLE IF EXISTS proprietario_carro;
+DROP TABLE IF EXISTS pecas_utilizadas;
+DROP TABLE IF EXISTS peca;
+DROP TABLE IF EXISTS armazem;
+DROP TABLE IF EXISTS carro;
+DROP TABLE IF EXISTS marca;
 DROP TABLE IF EXISTS cliente;
+DROP TABLE IF EXISTS localidade;
 
 
-
-
-DROP TABLE IF EXISTS modeloCarro_peca;
 
 CREATE TABLE carro  (
     id_carro INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -37,7 +36,7 @@ CREATE TABLE marca(
 CREATE TABLE modelo(
     id_modelo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     modelo VARCHAR(50),
-    marca_id INTEGER UNSIGNED,
+    id_marca INTEGER UNSIGNED,
     PRIMARY KEY (id_modelo)
 );
 
@@ -57,19 +56,19 @@ CREATE TABLE armazem(
 );
 
 CREATE TABLE pecas_utilizadas(
-    id_reparacao INTEGER UNSIGNED,
+    id_carro INTEGER UNSIGNED,
     id_peca INTEGER UNSIGNED,
     quantidade INTEGER 
 );
 
-ALTER TABLE pecas_utilizadas ADD CONSTRAINT PK_pecas_utilizadas PRIMARY KEY (id_reparacao, id_peca);
+ALTER TABLE pecas_utilizadas ADD CONSTRAINT PK_pecas_utilizadas PRIMARY KEY (id_carro, id_peca);
 
 CREATE TABLE reparacao(
     id_reparacao INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     id_carro INTEGER UNSIGNED,
     data_inicio DATE,
     date_fim DATE,
-    id_funcionario INTEGER,
+    id_funcionario INTEGER UNSIGNED,
     horas_funcionario INTEGER,
     PRIMARY KEY (id_reparacao)
 );
@@ -87,7 +86,8 @@ CREATE TABLE funcionario(
     telefone INTEGER,
     morada VARCHAR(50),
     id_especialidade INTEGER UNSIGNED,
-    PRIMARY KEY (id_funcionario)
+    id_localidade INTEGER UNSIGNED,
+    PRIMARY KEY (id_funcionario,id_localidade)
 );
 
 CREATE TABLE especialidade(
@@ -98,8 +98,8 @@ CREATE TABLE especialidade(
 );
 
 CREATE TABLE modeloCarro_peca(
-    id_modelo INTEGER UNSIGNED,
-    id_peca INTEGER UNSIGNED
+    id_modelo INTEGER UNSIGNED NOT NULL,
+    id_peca INTEGER UNSIGNED  NOT NULL
 );
 
 ALTER TABLE modeloCarro_peca ADD CONSTRAINT PK_modeloCarro_peca PRIMARY KEY (id_modelo, id_peca);
@@ -109,16 +109,57 @@ CREATE TABLE cliente(
     nome VARCHAR(50),
     telefone INTEGER,
     morada VARCHAR(50),
+    id_localidade INTEGER UNSIGNED,
     PRIMARY KEY (id_cliente)
 );
 
-ALTER TABLE carro ADD CONSTRAINT FK_carro_modelo FOREIGN KEY (id_modelo) REFERENCES modelo(id_modelo);
-ALTER TABLE carro ADD CONSTRAINT FK_carro_marca FOREIGN KEY (id_marca) REFERENCES marca(id_marca);
-ALTER TABLE carro ADD CONSTRAINT FK_carro_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
-ALTER TABLE funcionario ADD CONSTRAINT FK_funcionario_especialidade FOREIGN KEY (id_especialidade) REFERENCES especialidade(id_especialidade);
-ALTER TABLE funcionario_horasDeTrabalho ADD CONSTRAINT FK_funcionariohorasDetrabalho_funcionario FOREIGN KEY (id_funcionario) REFERENCES funcionario(id_funcionario);
-ALTER TABLE funcionario_horasDeTrabalho ADD CONSTRAINT FK_funcionariohorasDetrabalho_reparacao FOREIGN KEY (id_reparacao) REFERENCES reparacao(id_reparacao);
-ALTER TABLE pecas_utilizadas ADD CONSTRAINT FK_pecasUtilizadas_reparacao FOREIGN KEY (id_reparacao) REFERENCES reparacao(id_reparacao);
-ALTER TABLE pecas_utilizadas ADD CONSTRAINT FK_idPeca_peca FOREIGN KEY (id_peca) REFERENCES peca(id_peca);
-ALTER TABLE peca ADD CONSTRAINT FK_peca_armazem FOREIGN KEY (id_peca) REFERENCES armazem(id_armazem);
-ALTER TABLE modeloCarro_peca ADD CONSTRAINT FK_modeloCarro_modelo FOREIGN KEY (id_modelo) REFERENCES modelo(id_modelo);
+CREATE TABLE proprietario_carro(
+    id_carro INTEGER UNSIGNED,
+    id_cliente INTEGER UNSIGNED,
+    id_owner INTEGER UNSIGNED,
+    PRIMARY KEY (id_carro,id_cliente)
+);
+
+CREATE TABLE localidade(
+	id_localidade INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(50),
+	PRIMARY KEY (id_localidade)
+);
+
+
+ALTER TABLE proprietario_carro ADD CONSTRAINT FK_idCliente_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
+
+ALTER TABLE proprietario_carro ADD CONSTRAINT FK_idCarro_carro FOREIGN KEY (id_carro) REFERENCES carro(id_carro);
+
+ALTER TABLE proprietario_carro ADD CONSTRAINT FK_idOwner_cliente FOREIGN KEY (id_owner) REFERENCES cliente(id_cliente);
+
+
+ALTER TABLE carro ADD CONSTRAINT FK_idMarca_marca FOREIGN KEY (id_marca) REFERENCES marca(id_marca);
+
+
+ALTER TABLE modelo ADD CONSTRAINT FK_modeloIdMarca_marca FOREIGN KEY (id_marca) REFERENCES marca(id_marca);
+
+ALTER TABLE modeloCarro_peca ADD CONSTRAINT FK_idModelo_modelo FOREIGN KEY (id_modelo) REFERENCES modelo(id_modelo);
+
+ALTER TABLE modeloCarro_peca ADD CONSTRAINT FK_idPeca_peca FOREIGN KEY (id_peca) REFERENCES peca(id_peca);
+
+ALTER TABLE peca ADD CONSTRAINT FK_idArmazem_armazem FOREIGN KEY (id_armazem) REFERENCES armazem(id_armazem);
+
+
+ALTER TABLE pecas_utilizadas ADD CONSTRAINT FK_pecasUtilizadasIdCarro_carro FOREIGN KEY (id_carro) REFERENCES carro(id_carro);
+
+ALTER TABLE pecas_utilizadas ADD CONSTRAINT FK_pecasUtilizadasidPeca_peca FOREIGN KEY (id_peca) REFERENCES peca(id_peca);
+
+
+ALTER TABLE reparacao ADD CONSTRAINT FK_idCarro_PecasUtilizadas FOREIGN KEY (id_carro) REFERENCES pecas_utilizadas(id_carro);
+
+ALTER TABLE reparacao ADD CONSTRAINT FK_reparacaoIdFuncionario_idFuncionario FOREIGN KEY (id_funcionario) REFERENCES funcionario(id_funcionario);
+
+
+ALTER TABLE funcionario_horasDeTrabalho ADD CONSTRAINT FK_idReparacao_reparacao FOREIGN KEY (id_reparacao) REFERENCES reparacao(id_reparacao);
+
+ALTER TABLE funcionario_horasDeTrabalho ADD CONSTRAINT FK_idfuncionario_funcionario FOREIGN KEY (id_funcionario) REFERENCES funcionario(id_funcionario);
+
+ALTER TABLE funcionario ADD CONSTRAINT FK_idEspecialidade_especialidade FOREIGN KEY (id_especialidade) REFERENCES especialidade(id_especialidade);
+
+ALTER TABLE funcionario ADD CONSTRAINT FK_idLocalidade_localidade FOREIGN KEY (id_localidade) REFERENCES localidade(id_localidade);
